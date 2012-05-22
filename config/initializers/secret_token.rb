070ea2997fb4e2a5ff3351a500265bdc46372ca2
emerson <emerson@emerson-VirtualBox.(none)>
2012-05-22 16:52:48 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aa::Application.config.secret_token = '28154d0c95d554f269d5daa13020d06106023182a188a50837064f7a1339611d11923d06488b37c9f0fde183f2d75e8badbbe3c555fe02cdb98b146653db9fb2'
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Aa::Application.routes.draw do
+  resources :posts
+
+  get "home/index"
+
+
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+
+   root :to => 'home#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+end
